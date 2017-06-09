5703d690937f8f4229c97be02f431a2fe6eb036b
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-06-09 09:09:28 -0700

    initialize project files.

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	template:`
+	<div class="jumbotron">
+		<h1>Animal Tracker</h1>
+	</div>
+	<div class="container">
+			<h2>Animal List</h2>
+	</div>
+	`
+})
+
+export class AppComponent {
+
+}
